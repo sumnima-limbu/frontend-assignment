e2af5268413101445b8a715a60a6e9e0557bb7f1
sumnima-limbu <sumnimaklimbu@gmail.com>
2023-07-21 20:59:50 +0545

    add routes in the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import NavBar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import NoMatchPage from "./pages/NoMatch";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <NavBar />
-    </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<NoMatchPage />} />
+      </Routes>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -7,7 +8,10 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+
   // </React.StrictMode>
 );
 
